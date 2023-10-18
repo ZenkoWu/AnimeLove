eafa90b9915aff6560f6c5dd7bb2a7ed7aae0b6a
ZenkoWu <anna.zagrebaylo.03@mail.ru>
2023-10-18 20:34:41 +0300

    Update Header.js

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,15 +2,13 @@ import { useSelector } from 'react-redux';
 import s from './Header.module.css'
 import { NavLink, Navigate, useLocation, useNavigate } from "react-router-dom";
 
+const getRandomAnime = (navigate, totalAnimeCount) => {
+    const randomAnimeId = Math.floor(Math.random() * totalAnimeCount) + 1;
+    navigate(`anime/${randomAnimeId}`,{ replace: true })
+}
 const Header = () => {
-    // todo вынести рандом аниме кнопку в отдельный компонент для оптимизации
     const navigate = useNavigate();
     const totalAnimeCount = useSelector(state => state.animeList.totalAnimeCount)
-
-    const getRandomAnime = () => {
-        const randomAnimeId = Math.floor(Math.random() * (totalAnimeCount + 1))
-        navigate(`anime/${randomAnimeId}`,{ replace: true })
-    }
 
     return (
         <header className={`d-flex align-items-center justify-content-between ${s.container}`} style={{gap:'10px'}}>
@@ -20,7 +18,12 @@ const Header = () => {
                     <NavLink to='/anime' className={s.link}>Anime</NavLink>
                     <NavLink to='/manga' className={s.link}>Manga</NavLink>
                     <NavLink to='/characters' className={s.link}>Characters</NavLink>
-                    <p type ='button' className={`${s.link} m-0`} onClick={getRandomAnime}>Random anime</p>
+                    <p className={`${s.link} m-0`}
+                        onClick={()=>getRandomAnime(navigate, totalAnimeCount)}
+                        type='button'
+                    >
+                        Random anime
+                    </p>
                 </div>
             </div>
             <NavLink to='/favorites' className={s.title}>❤</NavLink>
