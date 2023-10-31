affa59220eea8725ecef76b87b7b72ea47e7029a
ZenkoWu <anna.zagrebaylo.03@mail.ru>
2023-11-01 01:30:11 +0300

    update TextFieldWrapper

--- src/components/Filter/TextFieldWrapper/TextFieldWrapper.tsx
+++ src/components/Filter/TextFieldWrapper/TextFieldWrapper.tsx
@@ -1,3 +1,4 @@
+import { flexPlace } from '@/utils/flexPlace'
 import s from './TextFieldWrapper.module.css'
 
 type TTextFieldWrapper = {
@@ -10,9 +11,8 @@ export const TextFieldWrapper = ({children, title, opened}: TTextFieldWrapper) =
     return (
         <div className='pb-16 m-0'>
             <p style={{padding: '5px 0'}}  className='m-0 h6'>{title}</p>
-            <div className={`d-flex align-items-center pointer rounded-3 ${s.padding} cursor-pointer ` + 
-                (opened ? s.border_red : s.border_default) 
-            }
+            <div 
+                className={`${s.fieldWrapper} ${opened ? s.border_red : s.border_default}`}
             >
                 {children}
             </div>
