fb685d961ad9784445f90848bb2db244a4c83903
ZenkoWu <anna.zagrebaylo.03@mail.ru>
2023-11-01 01:05:50 +0300

    update Modal

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,6 +1,5 @@
-import { useDispatch } from "react-redux";
 import s from './Modal.module.css';
-import { favoritesActions } from "../../redux/features/favorites";
+import { flexPlace } from "../../utils/flexPlace";
 
 type TDeleteTicket = {
     opened: boolean,
@@ -10,9 +9,13 @@ type TDeleteTicket = {
     title: string
 }
 
-export const Modal = ({setOpened, opened, onAccept, question, title}: TDeleteTicket) => {
-    const dispatch = useDispatch()
-    
+export const Modal = ({
+    setOpened, 
+    opened, 
+    onAccept, 
+    question, 
+    title
+}: TDeleteTicket) => {
     const onAcceptClick = () => {
         onAccept()
         setOpened(false)
@@ -31,7 +34,10 @@ export const Modal = ({setOpened, opened, onAccept, question, title}: TDeleteTic
             onClick={onRejection}
         >
             <div className={`bg-white rounded-3 p-4 ${s.modal}`}>
-                <div className='d-flex justify-content-between align-center pb-16 btn' onClick={()=> setOpened(false)}>
+                <div 
+                    className={`${flexPlace('between', 'center')} pb-16 btn`} 
+                    onClick={()=> setOpened(false)}
+                >
                     <p className='fs20 fw-600'>{title}</p>
                    x
                 </div>
@@ -46,7 +52,7 @@ export const Modal = ({setOpened, opened, onAccept, question, title}: TDeleteTic
                         Yes
                     </div>
                     <div 
-                        className={`cursor-pointer ${s.button} ${s.orange_border}`}
+                        className={`pointer ${s.button} ${s.orange_border}`}
                         onClick={onCancel} 
                     >
                         No
