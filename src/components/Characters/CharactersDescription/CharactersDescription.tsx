fd0459321b71e6162abb6c2f2cb779e2adcb0d92
ZenkoWu <anna.zagrebaylo.03@mail.ru>
2023-11-01 01:01:40 +0300

    update character description

--- src/components/Characters/CharactersDescription/CharactersDescription.tsx
+++ src/components/Characters/CharactersDescription/CharactersDescription.tsx
@@ -1,6 +1,6 @@
 import { useParams } from "react-router-dom"
 import Preloader from "../../Preloader/Preloader"
-import s from '../../Anime/AnimeDescription/AnimeDescription.module.css'
+import s from './CharactersDescription.module.css'
 import { TCharactersInfo } from "../../../types/mainElementsTypes"
 import { api } from "../../../redux/services/api/api"
 import FavoriteBtn from "../../Favorites/FavoriteBtn/FavoriteBtn"
@@ -27,39 +27,37 @@ export const CharactersDescription = () => {
         <div className={s.container}>
             <div className={s.cardBackground}>
                 <div className='p-4 w-100'>
-                <img src={characters.images.jpg.image_url} alt="anime-img" className={'pe-4 pb-2'} style={{float:'left'}}/>
-                <div className="pb-4" >
-                <div className="w-100"> 
-                    <div className={`${flexPlace('between', 'center')}`}>
-                        
-                        
-                        
-                    
-                        <h1 className='fw-bold fs-2' >{characters.name}</h1>
-                        
-                    <FavoriteBtn 
-                        category={API_ROUTES.CHARACTERS}
-                        info={{
-                            mal_id: characters.mal_id,
-                            images: characters.images,
-                            title: characters.name
-                        }}
+                    <img 
+                        src={characters.images.jpg.image_url} 
+                        alt="character-poster" 
+                        className={'pe-4 pb-2 float-start'} 
                     />
+                    <div className="pb-4" >
+                    <div className="w-100"> 
+                        <div className={`${flexPlace('between', 'center')}`}>
+                            <h1 className='fw-bold fs-2' >{characters.name}</h1>
+                            <FavoriteBtn 
+                                category={API_ROUTES.CHARACTERS}
+                                info={{
+                                    mal_id: characters.mal_id,
+                                    images: characters.images,
+                                    title: characters.name
+                                }}
+                            />
+                        </div>
+                        <p className={s.borderBottomGrey}>{characters.name_kanji}</p> 
+                        <p className="p-0 m-0">Nicknames: {characters.nicknames?.map(el => el).join(', ')}</p>
                     </div>
-                    <p className={s.borderBottomGrey}>{characters.name_kanji}</p> 
-                    <p className="p-0 m-0">Nicknames: {characters.nicknames?.map(el => el).join(', ')}</p>
-                </div>
 
-                    
-                { 
-                    characters.about &&
-                    <div className="py-3" >
-                        <h4>Description</h4>
-                        <p className="text-" style={{textAlign: 'justify'}}>{characters.about}</p>
+                        
+                    { 
+                        characters.about &&
+                        <div className="py-3">
+                            <h4>Description</h4>
+                            <p>{characters.about}</p>
+                        </div>
+                    }
                     </div>
-                }
-                </div>
-                   
                 </div>
             </div>
         </div>
