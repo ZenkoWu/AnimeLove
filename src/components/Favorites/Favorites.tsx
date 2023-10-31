8b6e132a4a2753149f39d1306f1abb22c5b8152d
ZenkoWu <anna.zagrebaylo.03@mail.ru>
2023-11-01 02:11:54 +0300

    Update Favorites.tsx

--- src/components/Favorites/Favorites.tsx
+++ src/components/Favorites/Favorites.tsx
@@ -7,16 +7,23 @@ import s from './Favorites.module.css'
 import { favoritesActions } from "../../redux/features/favorites"
 import {Modal} from "../Modal/Modal"
 import { TCategories } from "../../types/mainElementsTypes"
+import { flexPlace } from "../../utils/flexPlace"
 
 export const Favorites = () => {
     const [active, setActive]= useState<TCategories>('anime')
     const [opened, setOpened] = useState(false)
     const {favorites, count} = useSelector((state: TState) => selectFavoritesModule(state))
+
     const dispatch = useDispatch()
 
     const deleteFavorites = (category: TCategories) => {
         dispatch(favoritesActions.deleteFavorites(category))
     }
+    const tabs = [
+        {title: 'Anime', active: 'anime' },
+        {title: 'Manga', active: 'manga' },
+        {title: 'Characters', active: 'characters' },
+    ] as const;
 
     return (
         <div className={s.container}>
@@ -30,78 +37,59 @@ export const Favorites = () => {
                     title={'Delete favorite'}
                 />
             }
-                <div className="bg-white p-4">
-                    <h2>Favorites</h2>
-                    <div className=" d-flex justify-content-between">
-                        <ul className="nav nav-tabs w-100">
-                            <li className="nav-item cursor-pointer">
-                                <div 
-                                    className={`nav-link ${active == 'anime' && 'active'}`} 
-                                    aria-current="page" 
-                                    onClick={()=> setActive('anime')}
-                                >
-                                    Anime
-                                </div>
-                            </li>
-                            <li className="nav-item cursor-pointer">
-                                <div 
-                                    className={`nav-link ${active == 'manga' && 'active'}`} 
-                                    onClick={()=> setActive('manga')}
-                                >
-                                    Manga
-                                </div>
-                            </li>
-                            <li className="nav-item cursor-pointer">
-                                <div 
-                                    className={`nav-link ${active == 'characters' && 'active'}`} 
-                                    onClick={()=> setActive('characters')}
-                                >
-                                    Characters
-                                </div>
-                            </li>
-                        </ul>
-                        <button 
-                            onClick={()=> setOpened(true)} 
-                            className="btn btn-primary" 
-                            style={{whiteSpace:'nowrap'}}
-                            disabled={count === 0}
-                        >
-                            delete all {active}
-                        </button>
-                    </div>  
-                    <div className="pt-4">
-                        <div className="d-flex p-2 w-100 d-flex justify-between flex-wrap">
+            <div className="bg-white p-4">
+                <h2>Favorites</h2>
+                <div className={`${flexPlace('between')}`}>
+                    <ul className="nav nav-tabs w-100">
+                        {
+                            tabs.map(el => 
+                                <li className="nav-item cursor-pointer">
+                                    <div 
+                                        className={`nav-link ${active == el.active && 'active'}`} 
+                                        aria-current="page" 
+                                        onClick={()=> setActive(el.active)}
+                                    >
+                                        {el.title}
+                                    </div>
+                                </li>
+                            )
+                        }
+                    </ul>
+                    <button 
+                        onClick={()=> setOpened(true)} 
+                        className="btn btn-primary" 
+                        style={{whiteSpace:'nowrap'}}
+                        disabled={count === 0}
+                    >
+                        delete all {active}
+                    </button>
+                </div>  
+                <div className="pt-4"> 
+                    <div className={`d-flex p-2 w-100 flex-wrap`}> 
                         { 
                             active == 'manga' && favorites.manga?.count ?
                                 Object.values(favorites.manga.items).map(el => (
-                                // <div className=' red w-25'>
+                                // <div className=' red w-25'> // todo rewrite this code
                                     <ElementCard category={active} data={el}/>
                                     // </div>
                                 ))  
                             : 
-                            active == 'anime' && favorites.anime?.count ?
-                            Object.values(favorites.anime.items).map(el => (
-                            // <div className=' red w-25'>
-                                <ElementCard category={active} data={el}/>
-                                // </div>
-                            )) 
+                                active == 'anime' && favorites.anime?.count ?
+                                Object.values(favorites.anime.items).map(el => (
+                                    <ElementCard category={active} data={el}/>
+                                )) 
                             :
-                            active == 'characters' && favorites.characters?.count ?
-                            Object.values(favorites.characters.items).map(el => (
-                            // <div className=' red w-25'>
-                                <ElementCard category={active} data={el}/>
-                                // </div>
-                            ))  
+                                active == 'characters' && favorites.characters?.count ?
+                                Object.values(favorites.characters.items).map(el => (
+                                    <ElementCard category={active} data={el}/>
+                                ))  
                             : 
-                            <div>No favorite {active} </div>
+                                <div>No favorite {active} </div>
                             
                         }
-                        </div>
                     </div>
-                
-            
                 </div>
-            
+            </div>
         </div>
     )
 }
