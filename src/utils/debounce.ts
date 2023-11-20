518a5c476c6880799db0aaae3df00bdf0d8d6ff6
ZenkoWu <anna.zagrebaylo.03@mail.ru>
2023-11-20 18:01:05 +0300

    Update debounce.ts

--- src/utils/debounce.ts
+++ src/utils/debounce.ts
@@ -1,9 +1,8 @@
-
 // The function that “defers” calling another function until a certain amount of time has passed since 
 //the last call.
 
-export const debounce = ( callback: (e: React.SyntheticEvent)=> void, delay: number )=> {
-    let timeout: number;
+export const debounce = ( callback: (e: React.SyntheticEvent) => void, delay: number )=> {
+    let timeout: number | undefined;
     return (e: React.SyntheticEvent) => {
         clearTimeout( timeout );
         timeout = window.setTimeout(() => callback(e), delay );
